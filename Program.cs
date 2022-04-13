7a5a1ea130130d063d4f7a6b83473b086106dd7f
double_a <79718677+AA-Kl@users.noreply.github.com>
2022-04-14 02:51:51 +0300

    commit 14apr2022

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("hihi, its test of .net coding in vs code");
---
+++ test.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace IR_Curves_Construction // Note: namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hihi, its test of .net coding in vs code (old version)");
+        }
+    }
+}
